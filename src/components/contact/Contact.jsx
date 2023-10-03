9ae497291489850a7fd0e7b4dd97457f3fe3886c
hvgochr <charrade.hugo@gmail.com>
2023-10-03 17:10:50 +0200

    Contact button

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from "@hookform/resolvers/zod";
-import React from 'react';
-import "./contact.css";
+import React, { useEffect, useState } from 'react';
 import emailjs from 'emailjs-com';
+import "./contact.css";
 
 const SERVICE_ID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
 const TEMPLATE_ID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
@@ -28,7 +28,54 @@ const contactSchema = z.object({
     .nonempty('Message is required'),
 });
 
+const LoadingButton = () => (
+  <button className="button button--flex button--loading" id="contact__button" disabled>
+    <svg className="button__icon" viewBox="0 0 1024 1024">
+      <circle cx="512" cy="512" r="256" stroke="#fff" strokeWidth="32" fill="none" />
+      <path
+        className="button__icon-path"
+        fill="#fff"
+        d="M512 128a384 384 0 1 0 384 384A384 384 0 0 0 512 128zm0 640a256 256 0 1 1 256-256 256 256 0 0 1-256 256z"
+      />
+    </svg>
+    <span className="button__text">Loading</span>
+  </button>
+);
+
+const SuccessButton = () => (
+  <button className="button button--flex button--success" id="contact__button" disabled>
+    <svg className="button__icon" viewBox="0 0 1024 1024">
+      <path
+        className="button__icon-path"
+        fill="#fff"
+        d="M512 128a384 384 0 1 0 384 384A384 384 0 0 0 512 128zm192.5 256.5L448 633.8l-128.5-128.5-64.3 64.3L448 762.4l320.8-320.8-64.3-64.3z"
+      />
+    </svg>
+    <span className="button__text">Sent</span>
+  </button>
+);
+
+const ErrorButton = () => (
+  <button className="button button--flex button--error" id="contact__button" disabled>
+    <svg className="button__icon" viewBox="0 0 1024 1024">
+      <path
+        className="button__icon-path"
+        fill="#fff"
+        d="M512 128a384 384 0 1 0 384 384A384 384 0 0 0 512 128zm192.5 256.5L448 633.8l-128.5-128.5-64.3 64.3L448 762.4l320.8-320.8-64.3-64.3z"
+      />
+      <path
+        className="button__icon-path"
+        fill="#fff"
+        d="M448 320l128.5 128.5-128.5 128.5 45.3 45.3L622.2 448 512 337.8 402.8 448l28.5 28.5z"
+      />
+    </svg>
+    <span className="button__text">Error</span>
+  </button>
+);
+
 const Contact = () => {
+  const [buttonState, setButtonState] = useState('idle');
+
   const {
     register, 
     handleSubmit, 
@@ -36,14 +83,51 @@ const Contact = () => {
   } = useForm({resolver: zodResolver(contactSchema)});
 
   const onSubmit = (data) => {
-    console.log(data);
-    emailjs.send(SERVICE_ID, TEMPLATE_ID, data)
+    setButtonState('loading');
+    setTimeout(() => {
+      emailjs.send(SERVICE_ID, TEMPLATE_ID, data)
       .then((result) => {
+        setButtonState('success');
         console.log(result.text);
       }, (error) => {
+        setButtonState('error');
         console.log(error.text);
       });
+    }, 2000);
   };
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setButtonState('idle');
+    }, 60000);
+    return () => clearTimeout(timeoutId);
+  }, [buttonState]);
+
+  let buttonComponent;
+  switch (buttonState) {
+    case 'loading':
+      buttonComponent = <LoadingButton/>;
+      break;
+    case 'success':
+      buttonComponent = <SuccessButton/>;
+      break;
+    case 'error':
+      buttonComponent = <ErrorButton/>;
+      break;
+    default:
+      buttonComponent = (
+        <button type="submit" id="contact__button" className="button button--flex">
+          <svg className="button__icon" viewBox="0 0 24 24">
+            <path
+              className="button__icon-path"
+              fill="#fff"
+              d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L9,13.6L6.7,11.3c-0.4-0.4-1-0.4-1.4,0c-0.4,0.4-0.4,1,0,1.4l3,3c0.2,0.2,0.4,0.3,0.7,0.3s0.5-0.1,0.7-0.3l10-10C20.1,5.3,20.1,4.7,19.7,4.3z"
+            />
+          </svg>
+          <span className="button__text">Send</span>
+        </button>
+      );
+  }
 
   return (
     <section className="contact section" id="contact">
@@ -134,26 +218,7 @@ const Contact = () => {
                 <div className="contact__form-error">{errors.message.message}</div>
               )}
             </div>
-            <button id="contact__button" className="button button--flex">
-              Send Message
-              <svg
-                className="button__icon"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-              >
-                <path
-                  d="M14.2199 21.9352C13.0399 21.9352 11.3699 21.1052 10.0499 17.1352L9.32988 14.9752L7.16988 14.2552C3.20988 12.9352 2.37988 11.2652 2.37988 10.0852C2.37988 8.91525 3.20988 7.23525 7.16988 5.90525L15.6599 3.07525C17.7799 2.36525 19.5499 2.57525 20.6399 3.65525C21.7299 4.73525 21.9399 6.51525 21.2299 8.63525L18.3999 17.1252C17.0699 21.1052 15.3999 21.9352 14.2199 21.9352ZM7.63988 7.33525C4.85988 8.26525 3.86988 9.36525 3.86988 10.0852C3.86988 10.8052 4.85988 11.9052 7.63988 12.8252L10.1599 13.6652C10.3799 13.7352 10.5599 13.9152 10.6299 14.1352L11.4699 16.6552C12.3899 19.4352 13.4999 20.4252 14.2199 20.4252C14.9399 20.4252 16.0399 19.4352 16.9699 16.6552L19.7999 8.16525C20.3099 6.62525 20.2199 5.36525 19.5699 4.71525C18.9199 4.06525 17.6599 3.98525 16.1299 4.49525L7.63988 7.33525Z"
-                  fill="var(--container-color)"
-                ></path>
-                <path
-                  d="M10.11 14.7052C9.92005 14.7052 9.73005 14.6352 9.58005 14.4852C9.29005 14.1952 9.29005 13.7152 9.58005 13.4252L13.16 9.83518C13.45 9.54518 13.93 9.54518 14.22 9.83518C14.51 10.1252 14.51 10.6052 14.22 10.8952L10.64 14.4852C10.5 14.6352 10.3 14.7052 10.11 14.7052Z"
-                  fill="var(--container-color)"
-                ></path>
-              </svg>
-            </button> 
+            {buttonComponent}
           </form>
         </div>
       </div>
