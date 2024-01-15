2e0de837abb3761abc60598edfaa3c604e2960df
hvgochr <charrade.hugo@gmail.com>
2024-01-15 23:04:10 +0100

    Fix emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,9 +7,9 @@ import "./contact.css";
 
 const SERVICE_ID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
 const TEMPLATE_ID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
-const USER_ID = process.env.REACT_APP_EMAILJS_USER_ID;
+const PUBLIC_KEY = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
 
-emailjs.init(USER_ID);
+emailjs.init(PUBLIC_KEY);
 
 const contactSchema = z.object({
   name: z
